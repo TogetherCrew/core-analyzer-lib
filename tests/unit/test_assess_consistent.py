cfe1a3b65691f3ca9b45ab30447b22ed320c8c65
Mohammad Amin <dadgaramin96@gmail.com>
2023-08-17 17:24:50 +0330

    Fixed test function name!

--- tests/unit/test_assess_consistent.py
+++ tests/unit/test_assess_consistent.py
@@ -1,9 +1,9 @@
 from analyzer.utils.assessments import assess_consistent
 
 
-def test_assess_vital():
+def test_assess_consistent():
     """
-    test the assess_vital module
+    test the assess_consistent module
     """
     all_active = {
         "0": set(["user0", "user1", "user2", "user3", "user4"]),
@@ -12,12 +12,12 @@ def test_assess_vital():
         "0": set(),
     }
 
-    # For testing purposes we're setting 1 for each parameters of vital
+    # For testing purposes we're setting 1 for each parameters of consistent
     all_consistent = assess_consistent(
         all_active=all_active,
         w_i=1,
-        CON_O_THR=1,  # 1 time to be considered as vital
-        CON_T_THR=1,  # 1 time occured to be consired as vital
+        CON_O_THR=1,  # 1 time to be considered as consistent
+        CON_T_THR=1,  # 1 time occured to be consired as consistent
         WINDOW_D=7,
         all_consistent=all_consistent,
     )
